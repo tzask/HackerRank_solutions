ef6534cde3fae108e54a2030417b5d369440f9d1
Tetiana <tetiana.zaskorodnia@gmail.com>
2018-01-18 01:13:35 +0200

    Create Solution.java

---
+++ Java/Introduction - Java Stdin and Stdout II/Solution.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+//https://www.hackerrank.com/challenges/java-stdin-stdout/problem
+public class Solution {
+    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        double d = scan.nextDouble();
+        String s = scan.nextLine(); // because  because of this, the last newline character for that line of integer input is still
+                                    // queued in the input buffer
+        s = scan.nextLine();
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
