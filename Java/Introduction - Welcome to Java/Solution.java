9dcc0f0de83d70f96b0944f7ffaf67d8169268cc
Tetiana <tetiana.zaskorodnia@gmail.com>
2018-01-18 01:03:57 +0200

    Update Solution.java

--- Java/Introduction - Welcome to Java/Solution.java
+++ Java/Introduction - Welcome to Java/Solution.java
@@ -1,5 +1,6 @@
 public class Solution {
-
+    
+    //https://www.hackerrank.com/challenges/welcome-to-java/problem
     public static void main(String[] args) {
         System.out.println("Hello, World.");
         System.out.println("Hello, Java.");
