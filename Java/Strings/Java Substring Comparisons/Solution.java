91bbf4a888c2f451197330371136d7ac22a26d11
Tetiana <tetiana.zaskorodnia@gmail.com>
2018-01-22 02:00:00 +0200

    Create Solution.java

---
+++ Java/Strings/Java Substring Comparisons/Solution.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+//https://www.hackerrank.com/challenges/java-string-compare/problem
+public class Solution {
+  
+    public static String getSmallestAndLargest(String s, int k) {
+         /*  approx. O(n log n) ?
+             SortedSet<String> sets = new TreeSet<String>();
+                for(int i = 0;i <= s.length() - k; i++){
+                    sets.add(s.substring(i, i + k));
+                }
+             return sets.first() + "\n" + sets.last();
+        */
+        
+        // approx. O(n) ?
+        String smallest = s.substring(0, k); 
+        String largest = s.substring(0, k); 
+
+        for(int i = 1; i <= s.length() - k; i++) {
+            if ( s.substring(i, i + k).compareTo(smallest) < 0) {
+                smallest = s.substring(i, i + k);
+            } else if ( s.substring(i, i + k).compareTo(largest) > 0 ) {
+                largest = s.substring(i, i + k);
+            }
+        }
+        return smallest + "\n" + largest;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int k = scan.nextInt();
+        scan.close();
+      
+        System.out.println(getSmallestAndLargest(s, k));
+    }
+}
